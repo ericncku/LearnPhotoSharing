0484a36c13cfff0140a09718ebb05626f7c205a8
ericncku <erichoi0727@gmail.com>
2017-03-09 15:02:53 +0800

    Initial Commit

---
+++ photoSharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoSharing
+//
+//  Created by HOISIO LONG on 9/3/2017.
+//  Copyright © 2017年 Eric Hoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
